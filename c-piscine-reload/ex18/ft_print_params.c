f60f0054324be93628bcce5159a049d2bc84d095
Joan Fontbona Ardanaz <jfontbon@car7s2.42barcelona.com>
2025-05-05 11:36:26 +0200

    yes

--- c-piscine-reload/ex18/ft_print_params.c
+++ c-piscine-reload/ex18/ft_print_params.c
@@ -6,26 +6,32 @@
 /*   By: jfontbon <jfontbon@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 12:28:15 by jfontbon          #+#    #+#             */
-/*   Updated: 2025/05/05 11:19:53 by jfontbon         ###   ########.fr       */
+/*   Updated: 2025/05/05 11:30:26 by jfontbon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-int	main(int argc, char *argv[])
+void	print_str(char *str)
 {
-	int		i;
-	char	*ptr;
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
 
 	i = 1;
 	while (i < argc)
 	{
-		ptr = argv[i];
-		while (*ptr != '\0')
-		{
-			ft_putchar(ptr);
-			ptr++;
-		}
+		print_str(argv[i]);
 		ft_putchar('\n');
 		i++;
 	}
