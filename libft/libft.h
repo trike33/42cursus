154fc16c5daea9c0e8fa9734cd8d8b9553519e94
Joan Fontbona <joanfontbona@gmail.com>
2025-05-07 20:13:34 +0200

    add strchr and strrchr

--- libft/libft.h
+++ libft/libft.h
@@ -30,5 +30,6 @@ int		ft_toupper(int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
 #endif
